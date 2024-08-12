fa4069f93f7f7fd5c3eaf2c155e8754cf83314da
Marco131-xel <marcopoolchichesapon@gmail.com>
2024-08-11 23:01:38 -0600

    Geometricas

---
+++ Geometricas/src/main/java/Analizadores/Generador.java
@@ -0,0 +1,22 @@
+package Analizadores;
+
+public class Generador {
+    public static void main(String[] args) {
+        generar();
+    }
+    
+    public static void generar(){
+        try{
+            String ruta = "src/main/java/Analizadores/";
+            
+            String Flex[] = {ruta + "lexico.jflex", "-d", ruta};
+            jflex.Main.generate(Flex);
+            
+            String Cup[] = { "-destdir", ruta, "-parser", "parser", ruta + "sintactico.cup" };
+            java_cup.Main.main(Cup);
+            
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
--- Geometricas/src/main/java/com/mycompany/geometricas/Geometricas.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.geometricas;
-
-public class Geometricas {
-
-    public static void main(String[] args) {
-        System.out.println("Hola linux");
-    }
-}
